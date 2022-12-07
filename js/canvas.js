fd82d57d3df82923b93e844b31edf5063ef7dbe2
KyleMThornton <20633426+KyleMThornton@users.noreply.github.com>
2022-12-06 23:45:30 -0800

    Space background, pts.js implementation

--- js/canvas.js
+++ js/canvas.js
@@ -1,50 +1,25 @@
-const canvas = document.getElementById("canvas");
+Pts.quickStart( "#canvas", "#123" );
 
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#fff';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+var pts = [];
 
-// Create an array of balls, each with a random position and velocity
-const balls = [...Array(5)].map(() => {
-  return {
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    vx: Math.random() * 10 - 5,
-    vy: Math.random() * 10 - 5
-  };
-});
+space.add( { 
 
-// Set up the animation loop
-function draw() {
-  // Move each ball by its velocity
-  balls.forEach(ball => {
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+  // init with 500 random points 
+  start: (bound) => { pts = Create.distributeRandom( space.innerBound, 500 ); },
 
-    // Check if the ball has hit the edge of the canvas, and reverse its velocity if so
-    if (ball.x >= canvas.width || ball.x <= 0) {
-      ball.vx = -ball.vx;
+  animate: (time, ftime) => {
+      
+    let r = Math.abs( space.pointer.x-space.center.x )/space.center.x * 150 + 70;
+    let range = Circle.fromCenter( space.pointer, r );
+      
+    // check if each point is within circle's range
+    for (let i=0, len=pts.length; i<len; i++) {
+        form.fillOnly("#fff").point(pts[i], 0.5);
+      }
     }
-    if (ball.y >= canvas.height || ball.y <= 0) {
-      ball.vy = -ball.vy;
-    }
-  });
+  }
+);
 
-  // Clear the canvas and draw each ball in its new position
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  balls.forEach((ball, i) => {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, 5, 0, 2 * Math.PI); // Set the ball's radius to 5
-    ctx.fillStyle = ['#f00', '#0f0', '#00f', '#ff0', '#0ff'][i]; // Set the ball's color
-    ctx.strokeStyle = '#000'; // Set the ball's outline color
-    ctx.lineWidth = 2; // Set the ball's outline thickness
-    ctx.fill();
-    ctx.stroke(); // Draw the ball's outline
-  });
-
-  // Request another animation frame
-  requestAnimationFrame(draw);
-}
-
-// Start the animation loop
-requestAnimationFrame(draw);
+  
+  
+space.bindMouse().bindTouch().play();
